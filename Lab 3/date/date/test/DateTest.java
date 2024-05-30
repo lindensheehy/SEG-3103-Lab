01f21a5bfe6b5280d91c9c0582a3861a6be5a70b
lindensheehy <linden.sheehy@outlook.com>
2024-05-30 19:46:07 -0400

    Assets for Lab2 + All Lab3

---
+++ Lab 3/date/date/test/DateTest.java
@@ -0,0 +1,191 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+import org.junit.jupiter.api.Test;
+
+class DateTest {
+
+  String s = "dsad";
+
+  @Test
+  void nextDate_tc01() {
+    Date today = new Date(1700, 6, 20);
+    Date expectedTomorrow = new Date(1700, 6, 21);
+    assertEquals(expectedTomorrow, today.nextDate());
+  }
+
+  @Test
+  void nextDate_tc02() {
+    Date today = new Date(2005, 4, 15);
+    Date expectedTomorrow = new Date(2005, 4, 16);
+    assertEquals(expectedTomorrow, today.nextDate());
+  }
+
+  @Test
+  void nextDate_tc03() {
+    Date today = new Date(1901, 7, 20);
+    Date expectedTomorrow = new Date(1901, 7, 21);
+    assertEquals(expectedTomorrow, today.nextDate());
+  }
+
+  @Test
+  void nextDate_tc04() {
+    Date today = new Date(3456, 3, 27);
+    Date expectedTomorrow = new Date(3456, 3, 28);
+    assertEquals(expectedTomorrow, today.nextDate());
+  }
+
+  @Test
+  void nextDate_tc05() {
+    Date today = new Date(1500, 2, 17);
+    Date expectedTomorrow = new Date(1500, 2, 18);
+    assertEquals(expectedTomorrow, today.nextDate());
+  }
+
+  @Test
+  void nextDate_tc06() {
+    Date today = new Date(1700, 6, 29);
+    Date expectedTomorrow = new Date(1700, 6, 30);
+    assertEquals(expectedTomorrow, today.nextDate());
+  }
+
+  @Test
+  void nextDate_tc07() {
+    Date today = new Date(1800, 11, 29);
+    Date expectedTomorrow = new Date(1800, 11, 30);
+    assertEquals(expectedTomorrow, today.nextDate());
+  }
+
+  @Test
+  void nextDate_tc08() {
+    Date today = new Date(3453, 1, 29);
+    Date expectedTomorrow = new Date(3453, 1, 30);
+    assertEquals(expectedTomorrow, today.nextDate());
+  }
+
+  @Test
+  void nextDate_tc09() {
+    Date today = new Date(444, 2, 29);
+    Date expectedTomorrow = new Date(444, 3, 1);
+    assertEquals(expectedTomorrow, today.nextDate());
+  }
+
+  @Test
+  void nextDate_tc10() {
+    Date today = new Date(2005, 4, 30);
+    Date expectedTomorrow = new Date(2005, 5, 1);
+    assertEquals(expectedTomorrow, today.nextDate());
+  }
+
+  @Test
+  void nextDate_tc11() {
+    Date today = new Date(3453, 1, 30);
+    Date expectedTomorrow = new Date(3453, 1, 31);
+    assertEquals(expectedTomorrow, today.nextDate());
+  }
+
+  @Test
+  void nextDate_tc12() {
+    Date today = new Date(3456, 3, 30);
+    Date expectedTomorrow = new Date(3456, 3, 31);
+    assertEquals(expectedTomorrow, today.nextDate());
+  }
+
+  @Test
+  void nextDate_tc13() {
+    Date today = new Date(1901, 7, 31);
+    Date expectedTomorrow = new Date(1901, 8, 1);
+    assertEquals(expectedTomorrow, today.nextDate());
+  }
+
+  @Test
+  void nextDate_tc14() {
+    Date today = new Date(3453, 1, 31);
+    Date expectedTomorrow = new Date(3453, 2, 1);
+    assertEquals(expectedTomorrow, today.nextDate());
+  }
+
+  @Test
+  void nextDate_tc15() {
+    Date today = new Date(3456, 12, 31);
+    Date expectedTomorrow = new Date(3457, 1, 1);
+    assertEquals(expectedTomorrow, today.nextDate());
+  }
+
+  @Test
+  void nextDate_invalid_tc16() {
+    assertThrows(
+      IllegalArgumentException.class,
+      () -> new Date(1500, 2, 31)
+    );
+  }
+
+  @Test
+  void nextDate_invalid_tc17() {
+    assertThrows(
+      IllegalArgumentException.class,
+      () -> new Date(1500, 2, 29)
+    );
+  }
+
+  @Test
+  void nextDate_invalid_tc18() {
+    assertThrows(
+      IllegalArgumentException.class,
+      () -> new Date(-1, 10, 20)
+    );
+  }
+
+  @Test
+  void nextDate_invalid_tc19() {
+    assertThrows(
+      IllegalArgumentException.class,
+      () -> new Date(1458, 15, 12)
+    );
+  }
+
+  @Test
+  void nextDate_invalid_tc20() {
+    assertThrows(
+      IllegalArgumentException.class,
+      () -> new Date(1975, 6, -50)
+    );
+  }
+
+  @Test
+  void dateToStringTest() {
+    Date date = new Date(2000, 4, 5);
+    assertTrue(date.toString().equals("2000/April/5"));
+  }
+
+  @Test
+  void dateEqualsTest1() {
+    Date date = new Date(2000, 4, 5);
+    String other = new String("abc");
+    assertFalse(date.equals(other));
+  }
+
+  @Test
+  void dateEqualsTest2() {
+    Date date = new Date(2000, 4, 5);
+    Date other = new Date(2001, 4, 5);
+    assertFalse(date.equals(other));
+  }
+
+  @Test
+  void dateEqualsTest3() {
+    Date date = new Date(2000, 4, 5);
+    Date other = new Date(2000, 5, 5);
+    assertFalse(date.equals(other));
+  }
+
+  @Test
+  void dateEqualsTest4() {
+    Date date = new Date(2000, 4, 5);
+    Date other = new Date(2000, 4, 5);
+    assertTrue(date.equals(other));
+  }
+
+}
