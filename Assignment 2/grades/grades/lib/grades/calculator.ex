bb3430648230bf7b39de7c32b48106a2e6041830
lindensheehy <linden.sheehy@outlook.com>
2024-07-02 15:01:27 -0400

    refactored calculator.ex

--- Assignment 2/grades/grades/lib/grades/calculator.ex
+++ Assignment 2/grades/grades/lib/grades/calculator.ex
@@ -1,38 +1,28 @@
 defmodule Grades.Calculator do
-  def percentage_grade(%{homework: homework, labs: labs, midterm: midterm, final: final}) do
-    avg_homework =
-      if Enum.count(homework) == 0 do
-        0
-      else
-        Enum.sum(homework) / Enum.count(homework)
-      end
 
-    avg_labs =
-      if Enum.count(labs) == 0 do
-        0
-      else
-        Enum.sum(labs) / Enum.count(labs)
-      end
+  def avg(%{input: input}) do
 
-    mark = 0.2 * avg_labs + 0.3 * avg_homework + 0.2 * midterm + 0.3 * final
-    round(mark * 100)
+    try do
+      Enum.sum(input) / Enum.count(input)
+    rescue
+      0
+    end
+
   end
 
-  def letter_grade(%{homework: homework, labs: labs, midterm: midterm, final: final}) do
-    avg_homework =
-      if Enum.count(homework) == 0 do
-        0
-      else
-        Enum.sum(homework) / Enum.count(homework)
-      end
+  def percentage_grade(%{homework: homework, labs: labs, midterm: midterm, final: final}) do
 
-    avg_labs =
-      if Enum.count(labs) == 0 do
-        0
-      else
-        Enum.sum(labs) / Enum.count(labs)
-      end
+    avg_homework = avg(%{input: homework})
+    avg_labs = avg(%{input: labs})
 
+    round((0.2 * avg_labs + 0.3 * avg_homework + 0.2 * midterm + 0.3 * final) * 100)
+
+  end
+
+  def grade(%{homework: homework, labs: labs, midterm: midterm, final: final}) do
+
+    avg_homework = avg(%{input: homework})
+    avg_labs = avg(%{input: labs})
     avg_exams = (midterm + final) / 2
 
     num_labs =
@@ -43,64 +33,49 @@ defmodule Grades.Calculator do
     if avg_homework < 0.4 || avg_exams < 0.4 || num_labs < 3 do
       "EIN"
     else
-      mark = 0.2 * avg_labs + 0.3 * avg_homework + 0.2 * midterm + 0.3 * final
-
-      cond do
-        mark > 0.895 -> "A+"
-        mark > 0.845 -> "A"
-        mark > 0.795 -> "A-"
-        mark > 0.745 -> "B+"
-        mark > 0.695 -> "B"
-        mark > 0.645 -> "C+"
-        mark > 0.595 -> "C"
-        mark > 0.545 -> "D+"
-        mark > 0.495 -> "D"
-        mark > 0.395 -> "E"
-        :else -> "F"
-      end
+      0.2 * avg_labs + 0.3 * avg_homework + 0.2 * midterm + 0.3 * final
     end
+
+  end
+
+  def letter_grade(%{homework: homework, labs: labs, midterm: midterm, final: final}) do
+
+    mark = grade(%{homework: homework, labs: labs, midterm: midterm, final: final})
+
+    cond do
+      mark > 0.895 -> "A+"
+      mark > 0.845 -> "A"
+      mark > 0.795 -> "A-"
+      mark > 0.745 -> "B+"
+      mark > 0.695 -> "B"
+      mark > 0.645 -> "C+"
+      mark > 0.595 -> "C"
+      mark > 0.545 -> "D+"
+      mark > 0.495 -> "D"
+      mark > 0.395 -> "E"
+      :else -> "F"
+    end
+
   end
 
   def numeric_grade(%{homework: homework, labs: labs, midterm: midterm, final: final}) do
-    avg_homework =
-      if Enum.count(homework) == 0 do
-        0
-      else
-        Enum.sum(homework) / Enum.count(homework)
-      end
 
-    avg_labs =
-      if Enum.count(labs) == 0 do
-        0
-      else
-        Enum.sum(labs) / Enum.count(labs)
-      end
+    mark = grade(%{homework: homework, labs: labs, midterm: midterm, final: final})
 
-    avg_exams = (midterm + final) / 2
-
-    num_labs =
-      labs
-      |> Enum.reject(fn mark -> mark < 0.25 end)
-      |> Enum.count()
-
-    if avg_homework < 0.4 || avg_exams < 0.4 || num_labs < 3 do
-      0
-    else
-      mark = 0.2 * avg_labs + 0.3 * avg_homework + 0.2 * midterm + 0.3 * final
-
-      cond do
-        mark > 0.895 -> 10
-        mark > 0.845 -> 9
-        mark > 0.795 -> 8
-        mark > 0.745 -> 7
-        mark > 0.695 -> 6
-        mark > 0.645 -> 5
-        mark > 0.595 -> 4
-        mark > 0.545 -> 3
-        mark > 0.495 -> 2
-        mark > 0.395 -> 1
-        :else -> 0
-      end
+    cond do
+      mark > 0.895 -> 10
+      mark > 0.845 -> 9
+      mark > 0.795 -> 8
+      mark > 0.745 -> 7
+      mark > 0.695 -> 6
+      mark > 0.645 -> 5
+      mark > 0.595 -> 4
+      mark > 0.545 -> 3
+      mark > 0.495 -> 2
+      mark > 0.395 -> 1
+      :else -> 0
     end
+
   end
+
 end
